4c34adc1170d979eac4be9b98f93c7f57bf09d74
Ren Wang <iamwrm@gmail.com>
2018-09-28 17:40:54 +0800

    add set_extend_multiple function

--- main.cpp
+++ main.cpp
@@ -17,22 +17,25 @@ class My_Array {
 	T* Head_p;
 	long long int length_limit;
 	long long int curr_length;
+	int extend_multiple;
 
        public:
 	void debug_print()
 	{
+		cout << "\n\n---------------------\n";
 		cout << "Head p addr:\t" << Head_p << endl;
 		cout << "length_limit:\t" << length_limit << endl;
 		cout << "curr_length:\t" << curr_length << endl;
 		print_all();
 	}
-	My_Array()
+	My_Array(my_array_index_type input_length_limit = 1024)
 	{
-		length_limit = 8;
+		length_limit = input_length_limit;
+		extend_multiple = 2;
 		Head_p = new T[length_limit];
 		curr_length = 0;
 	}
-	T At(my_array_index_type index)
+	T at(my_array_index_type index)
 	{
 		if (index < curr_length) {
 			return *(Head_p + index);
@@ -40,9 +43,18 @@ class My_Array {
 			throw 1;
 		}
 	}
-	void extend_array()
+	void change_at(my_array_index_type index, T input_elt)
 	{
-		T* new_array = new T[length_limit * 2];
+		if (index < curr_length) {
+			*(Head_p + index) = input_elt;
+		} else {
+			throw 1;
+		}
+	}
+	void _extend_array()
+	{
+		int length_multiple = 2;
+		T* new_array = new T[length_limit * length_multiple];
 		for (my_array_index_type i = 0; i < curr_length; i++) {
 			*(new_array + i) = *(Head_p + i);
 		}
@@ -56,7 +68,7 @@ class My_Array {
 			*(Head_p + curr_length++) = elt;
 		} else {
 			// TODO: enlarge
-			extend_array();
+			_extend_array();
 			push_back(elt);
 		}
 	}
@@ -67,6 +79,12 @@ class My_Array {
 		}
 		cout << endl;
 	}
+	void set_extend_multiple(int input)
+	{
+		if (input > 1) {
+			extend_multiple = input;
+		}
+	}
 
 	~My_Array()
 	{
@@ -76,9 +94,17 @@ class My_Array {
 
 int main()
 {
+	std::clock_t start;
+	double duration;
+
+	start = std::clock();
+
+	/* Your algorithm here */
+
 	string temp_line;
 
-	My_Array<my_int_type> my_array_1;
+	My_Array<my_int_type> my_array_1(8);
+	my_array_1.set_extend_multiple(2);
 
 	while (getline(cin, temp_line)) {
 		if (temp_line.length() == 0) {
@@ -92,13 +118,13 @@ int main()
 
 	my_array_1.debug_print();
 
-	int num = 10;
-	for (int i = 0; i < num; i++) {
+	long long num = 1000000;
+	for (long long i = 0; i < num; i++) {
 		my_array_1.push_back(i);
-		my_array_1.debug_print();
 	}
 
-	my_array_1.debug_print();
+	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
 
+	std::cout << "printf: " << duration << '\n';
 	return 0;
 }
