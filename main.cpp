ba1fcffc9b7c1ff912b56f0ab4658fb75efcb779
Ren Wang <iamwrm@gmail.com>
2018-11-07 19:08:45 +0800

    finish  ./main --verbose -i UNSORTED

--- main.cpp
+++ main.cpp
@@ -14,19 +14,14 @@ int main(int argc, char **argv)
 		static struct option long_options[] = {
 		    /* These options set a flag. */
 		    {"verbose", no_argument, &verbose_flag, 1},
-		    {"brief", no_argument, &verbose_flag, 0},
 		    /* These options don’t set a flag.
 		       We distinguish them by their indices. */
-		    {"add", no_argument, 0, 'a'},
-		    {"append", no_argument, 0, 'b'},
-		    {"delete", required_argument, 0, 'd'},
-		    {"create", required_argument, 0, 'c'},
-		    {"file", required_argument, 0, 'f'},
+		    {"im", required_argument, 0, 'i'},
 		    {0, 0, 0, 0}};
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 
-		c = getopt_long(argc, argv, "abc:d:f:", long_options,
+		c = getopt_long(argc, argv, "i:", long_options,
 				&option_index);
 
 		/* Detect the end of the options. */
@@ -42,27 +37,9 @@ int main(int argc, char **argv)
 				if (optarg) printf(" with arg %s", optarg);
 				printf("\n");
 				break;
-
-			case 'a':
-				puts("option -a\n");
+			case 'i':
+				printf("option -i with value `%s'\n", optarg);
 				break;
-
-			case 'b':
-				puts("option -b\n");
-				break;
-
-			case 'c':
-				printf("option -c with value `%s'\n", optarg);
-				break;
-
-			case 'd':
-				printf("option -d with value `%s'\n", optarg);
-				break;
-
-			case 'f':
-				printf("option -f with value `%s'\n", optarg);
-				break;
-
 			case '?':
 				/* getopt_long already printed an error message.
 				 */
@@ -84,6 +61,9 @@ int main(int argc, char **argv)
 		while (optind < argc) printf("%s ", argv[optind++]);
 		putchar('\n');
 	}
+
+
+
 
 	exit(0);
 }
