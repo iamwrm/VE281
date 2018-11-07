4aed4e750188f166952eb6b5bc0d8374f44c715c
Ren Wang <iamwrm@gmail.com>
2018-11-07 22:28:04 +0800

    import 4 .h files

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@
 
 int main(int argc, char **argv)
 {
+	// for I/O speedup ============================
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	// ============================================
+
+	// get verbose_flag and method_name ===========
 	int verbose_flag = 0;
 	std::string method_name;
-
 	get_ops(argc, argv, verbose_flag, method_name);
+	// ============================================
 
 	if (verbose_flag) puts("verbose flag is set");
 	std::cout << "method chosen:" << method_name << std::endl;
