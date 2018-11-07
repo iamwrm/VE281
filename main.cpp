1db3e5e4010e7c7fc9bf28f68ab24e0a5c01e812
Ren Wang <iamwrm@gmail.com>
2018-11-07 19:26:20 +0800

    cleaning code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-
 #include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
 /* Flag set by ‘--verbose’. */
-static int verbose_flag;
 
-int main(int argc, char **argv)
+void get_ops(int argc, char **argv, int &verbose_flag, std::string &method_name)
 {
 	int c;
 
@@ -16,13 +16,12 @@ int main(int argc, char **argv)
 		    {"verbose", no_argument, &verbose_flag, 1},
 		    /* These options don’t set a flag.
 		       We distinguish them by their indices. */
-		    {"im", required_argument, 0, 'i'},
+		    {"implementation", required_argument, 0, 'i'},
 		    {0, 0, 0, 0}};
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 
-		c = getopt_long(argc, argv, "i:", long_options,
-				&option_index);
+		c = getopt_long(argc, argv, "i:", long_options, &option_index);
 
 		/* Detect the end of the options. */
 		if (c == -1) break;
@@ -32,13 +31,17 @@ int main(int argc, char **argv)
 				/* If this option set a flag, do nothing else
 				 * now. */
 				if (long_options[option_index].flag != 0) break;
+				/*
 				printf("option %s",
 				       long_options[option_index].name);
 				if (optarg) printf(" with arg %s", optarg);
 				printf("\n");
+				*/
 				break;
 			case 'i':
-				printf("option -i with value `%s'\n", optarg);
+				// printf("option -i with value `%s'\n",
+				// optarg);
+				method_name = optarg;
 				break;
 			case '?':
 				/* getopt_long already printed an error message.
@@ -50,20 +53,23 @@ int main(int argc, char **argv)
 		}
 	}
 
-	/* Instead of reporting ‘--verbose’
-	   and ‘--brief’ as they are encountered,
-	   we report the final status resulting from them. */
-	if (verbose_flag) puts("verbose flag is set");
-
 	/* Print any remaining command line arguments (not options). */
 	if (optind < argc) {
-		printf("non-option ARGV-elements: ");
-		while (optind < argc) printf("%s ", argv[optind++]);
-		putchar('\n');
+		//printf("non-option ARGV-elements: ");
+		//while (optind < argc) printf("%s ", argv[optind++]);
+		//putchar('\n');
 	}
+}
 
+int main(int argc, char **argv)
+{
+	int verbose_flag = 0;
+	std::string method_name;
 
+	get_ops(argc, argv, verbose_flag, method_name);
 
+	if (verbose_flag) puts("verbose flag is set");
+	std::cout << "method chosen:" << method_name << std::endl;
 
-	exit(0);
+return 0;
 }
