80b2f50cb2bd38cfbe4f16b4343f73e6ddc110ed
Ren Wang <iamwrm@gmail.com>
2018-11-29 19:48:41 +0800

    deal process finish

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:	main.cpp ut.cpp ut.h
-	clang++ -o main -std=c++11 main.cpp ut.cpp
+	clang++ -o main -std=c++11 main.cpp ut.cpp -g
--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include "ut.h"
 
-
-
 struct Pool {
 	std::map<std::string, int> curr_e_names;
 
@@ -29,6 +27,112 @@ void put_in_ve(Pool &pool, One_Line_Order &olo)
 		the_equity.sell_pq.emplace(
 		    std::move(Order_for_pq(olo.ID, olo.price)));
 	}
+}
+
+bool find_buyer_and_trade(Pool &pool, One_Line_Order &olo,
+			  const int &cur_time_stamp)
+{
+	std::string e_name = olo.e_name;
+	auto it = pool.curr_e_names.find(e_name);
+	if (it == pool.curr_e_names.end()) {
+		return false;
+	}
+	// find the equity
+	int id_of_e = it->second;
+	auto &the_pq = pool.ve[id_of_e].buy_pq;
+
+	if (olo.number == 0) {
+		return true;
+	}
+
+	while (!the_pq.empty()) {
+		if (olo.number == 0) {
+			return true;
+		}
+		int top_order_id = the_pq.top().ID;
+
+		One_Line_Order &olo_pq = pool.va[top_order_id].olo;
+
+		if (olo_pq.expire_time != -1 &&
+		    olo_pq.expire_time + olo_pq.time_stamp < cur_time_stamp) {
+			// expired
+			the_pq.pop();
+			continue;
+		}
+
+		if (olo_pq.price < olo.price) {
+			// the highest price is lower than wanted
+			return false;
+		}
+
+		if (olo.number >= olo_pq.number) {
+			// DONE: trade event
+			olo.number -= olo_pq.number;
+			olo_pq.number = 0;
+			the_pq.pop();
+			continue;
+		} else {
+			// DONE: trade event
+			olo_pq.number -= olo.number;
+			olo.number = 0;
+			return true;
+		}
+	}
+	// sell to all pq, still have some left
+	return false;
+}
+
+bool find_seller_and_trade(Pool &pool, One_Line_Order &olo,
+			   const int &cur_time_stamp)
+{
+	std::string e_name = olo.e_name;
+	auto it = pool.curr_e_names.find(e_name);
+	if (it == pool.curr_e_names.end()) {
+		return false;
+	}
+	// find the equity
+	int id_of_e = it->second;
+	auto &the_pq = pool.ve[id_of_e].sell_pq;
+
+	if (olo.number == 0) {
+		return true;
+	}
+
+	while (!the_pq.empty()) {
+		if (olo.number == 0) {
+			return true;
+		}
+		int top_order_id = the_pq.top().ID;
+
+		One_Line_Order &olo_pq = pool.va[top_order_id].olo;
+
+		if (olo_pq.expire_time != -1 &&
+		    olo_pq.expire_time + olo_pq.time_stamp < cur_time_stamp) {
+			// expired
+			the_pq.pop();
+			continue;
+		}
+
+		if (olo_pq.price > olo.price) {
+			// the lowest price is higher than wanted
+			return false;
+		}
+
+		if (olo.number >= olo_pq.number) {
+			// DONE: trade event
+			olo.number -= olo_pq.number;
+			olo_pq.number = 0;
+			the_pq.pop();
+			continue;
+		} else {
+			// DONE: trade event
+			olo_pq.number -= olo.number;
+			olo.number = 0;
+			return true;
+		}
+	}
+	// buy all pq, still have some left
+	return false;
 }
 
 int main(int argc, char **argv)
@@ -64,6 +168,20 @@ int main(int argc, char **argv)
 		}
 
 		// match it
+		bool trade_success;
+		if (olo.is_buy) {
+			trade_success = find_seller_and_trade(
+			    pool, olo, current_time_stamp);
+		} else {
+			trade_success =
+			    find_buyer_and_trade(pool, olo, current_time_stamp);
+		}
+		if (trade_success) {
+			continue;
+		}
+		if (olo.expire_time == 0) {
+			continue;
+		}
 
 		// put in va
 		pool.va.emplace_back(std::move(Argu_Order(olo, 1)));
