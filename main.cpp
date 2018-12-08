8601e8fa197b8ee6f3f8357124177d711ddf02b0
Ren Wang <iamwrm@gmail.com>
2018-12-08 23:01:30 +0800

    is dag finished

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iomanip>
 #include <iostream>
 #include <limits>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
+
+#define MAX_INT std::numeric_limits<int>::max()
 
 using std::cin;
 using std::cout;
@@ -11,14 +14,39 @@ using std::endl;
 using std::string;
 using std::vector;
 
+class Node {
+	bool is_visited = false;
+
+       public:
+};
+
 class Graph {
 	vector<vector<int> > arr;
+	vector<Node> node_list;
 	int num;
+	bool cyc_chk(int v, std::set<int> s)
+	{
+		if (s.find(v) != s.end()) {
+			return true;
+		}
+		s.insert(v);
+		for (int i = 0; i < num; i++) {
+			if (arr[v][i] < MAX_INT) {
+				if (cyc_chk(i, s)) {
+					return true;
+				} else {
+					s.erase(i);
+				}
+			}
+		}
+		return false;
+	}
 
        public:
 	Graph(int size)
 	    : num(size),
-	      arr(size, vector<int>(size, std::numeric_limits<int>::max()))
+	      arr(size, vector<int>(size, MAX_INT)),
+	      node_list(size, Node())
 	{
 	}
 	void print_graph()
@@ -34,24 +62,34 @@ class Graph {
 	{
 		arr[from][to] = weight;
 	}
+	bool is_dag()
+	{
+		for (int i = 0; i < num; i++) {
+			if (cyc_chk(i, std::set<int>())) {
+				return false;
+			}
+		}
+		return true;
+	}
 };
 
 int main()
 {
 	int size;
 	cin >> size;
-	// cout << size;
 	Graph g1(size);
 
 	string line;
 	while (getline(cin, line)) {
-		// cout << line << endl;
+		if (line.empty()) continue;
 		std::stringstream ss1(line);
 		int from, to, weight;
 		ss1 >> from >> to >> weight;
 		g1.add_edge(from, to, weight);
 	}
 	g1.print_graph();
+
+	cout << "\nisdag:" << g1.is_dag();
 
 	return 0;
 }
