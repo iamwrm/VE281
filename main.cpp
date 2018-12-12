fc3b805077732a9a5202ad558da0d1eb539468e2
Ren Wang <iamwrm@gmail.com>
2018-12-12 09:17:53 +0800

    bug

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <queue>
 #include <set>
 #include <sstream>
 #include <string>
@@ -26,7 +27,9 @@ class Edge {
        public:
 	int from;
 	int to;
-	Edge(int f, int t) : from(f), to(t)
+	int weight;
+	typedef std::shared_ptr<Edge> Ptr;
+	Edge(int f, int t, int w) : from(f), to(t), weight(w)
 	{
 	}
 };
@@ -35,8 +38,18 @@ class Graph {
 	vector<vector<int> > arr;
 	std::shared_ptr<vector<Edge> > edge_list_ptr;
 	std::shared_ptr<vector<Node> > node_list_ptr;
+	vector<std::shared_ptr<Edge> > edge_ptr_pool;
+	vector<bool> if_spanned;
 	int num;
 	bool _has_mst = false;
+	int mst_val = 0;
+	struct greater_t {
+		bool operator()(const Edge::Ptr x, const Edge::Ptr y) const
+		{
+			return x->weight > y->weight;
+		}
+	};
+	std::priority_queue<Edge::Ptr, vector<Edge::Ptr>, greater_t> pq1;
 
 	bool cyc_chk(int v, std::set<int> s)
 	{
@@ -71,17 +84,29 @@ class Graph {
 	void add_edge(int from, int to, int weight)
 	{
 		arr[from][to] = weight;
-		edge_list_ptr->push_back((Edge(from, to)));
+		edge_list_ptr->push_back(Edge(from, to, weight));
+
+		if (!edge_ptr_pool[from * num + to]) {
+			edge_ptr_pool[from * num + to] =
+			    std::make_shared<Edge>(from, to, weight);
+		}
 	}
 	void convert_to_undirected()
 	{
 		for (auto e : *edge_list_ptr) {
 			int f = e.from;
 			int t = e.to;
-			if (arr[t][f] < arr[f][t]) {
-				arr[t][f] = arr[f][t];
-			} else {
-				arr[f][t] = arr[t][f];
+			cout << f << t << arr[f][t] << endl;
+			if (arr[f][t] < MAX_INT) {
+				if (arr[t][f] < arr[f][t]) {
+					arr[f][t] = arr[t][f];
+					edge_ptr_pool[f * num + t] =
+					    edge_ptr_pool[t * num + f];
+				} else {
+					arr[t][f] = arr[f][t];
+					edge_ptr_pool[t * num + f] =
+					    edge_ptr_pool[f * num + t];
+				}
 			}
 		}
 	}
@@ -89,17 +114,69 @@ class Graph {
 	{
 		node_list_ptr->push_back(Node(from));
 	}
+
+	void add_all_edge_tobe(int i)
+	{
+		if (if_spanned[i] == true) {
+			for (int j = 0; j < num; j++) {
+				if (arr[i][j] < MAX_INT) {
+					if (if_spanned[i] == 0) {
+						if (edge_ptr_pool[i * num +
+								  j]) {
+							cout << i << j;
+							pq1.emplace(
+							    edge_ptr_pool
+								[i * num + j]);
+						}
+					}
+				}
+			}
+		}
+	}
 	void prim()
 	{
 		_has_mst = false;
+
+		if_spanned[0] = 1;
+
+		int spanned_num = 1;
+
+		for (int k = 0; k < num - 1; k++) {
+			for (int i = 0; i < num; i++) {
+				add_all_edge_tobe(i);
+			}
+			if (pq1.empty()) {
+				continue;
+			}
+			Edge::Ptr shortest_edge = pq1.top();
+			if (!shortest_edge) {
+				continue;
+			}
+			if_spanned[shortest_edge->to] = 1;
+			mst_val += shortest_edge->weight;
+			spanned_num++;
+			while (!pq1.empty()) {
+				pq1.pop();
+			}
+		}
+		if (spanned_num != num) {
+			_has_mst = false;
+		} else {
+			_has_mst = true;
+		}
 	}
 
        public:
-	Graph(int size) : num(size), arr(size, vector<int>(size, MAX_INT))
+	Graph(int size)
 	//,node_list(size, Node())
 	{
+		num = size;
+		arr = vector<vector<int> >(size, vector<int>(size, MAX_INT));
 		edge_list_ptr = std::make_shared<vector<Edge> >();
 		node_list_ptr = std::make_shared<vector<Node> >();
+		edge_ptr_pool =
+		    vector<std::shared_ptr<Edge> >(num * num, nullptr);
+		if_spanned = vector<bool>(num, false);
 	}
 	void print_graph()
 	{
@@ -133,6 +210,7 @@ class Graph {
 	void print_mst()
 	{
 		convert_to_undirected();
+		print_graph();
 		prim();
 		if (has_mst()) {
 			cout << "The total weight of MST is " << 12 << "\n";
@@ -152,6 +230,7 @@ int main()
 
 	g1.read_from_cin();
 	g1.print_is_dag();
+	g1.print_graph();
 
 	g1.print_mst();
 
